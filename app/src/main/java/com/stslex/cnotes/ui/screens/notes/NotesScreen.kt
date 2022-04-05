fc262c7a7aaa20a7859e3a6adec38bf7272fd570
stslex <ilya977.077@gmail.com>
2022-04-05 19:43:37 +0300

    top app bar

--- app/src/main/java/com/stslex/cnotes/ui/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/stslex/cnotes/ui/screens/notes/NotesScreen.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.*
+import androidx.compose.material3.TopAppBarDefaults.enterAlwaysScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -37,13 +40,19 @@ fun NotesScreen(
         mutableStateListOf<NoteUIModel>()
     }
     val deleteNotesFunction = viewModel::deleteNotesById
+
+    val scrollBehavior = enterAlwaysScrollBehavior { true }
+
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         floatingActionButton = notesFab(
             lazyListState = lazyListState,
             selectedNotes = selectedNotes,
             deleteNotesFunction = deleteNotesFunction,
             navController = navController
-        )
+        ),
+        topBar = noteMediumTopAppBar(scrollBehavior),
+        floatingActionButtonPosition = FabPosition.End
     ) { paddingValues ->
         Surface(
             modifier = Modifier.padding(paddingValues)
@@ -63,6 +72,36 @@ fun NotesScreen(
             }
         }
     }
+}
+
+
+@ExperimentalMaterial3Api
+fun noteMediumTopAppBar(scrollBehavior: TopAppBarScrollBehavior): @Composable () -> Unit = {
+    MediumTopAppBar(
+        title = {
+            Text(
+                text = "Notes",
+                style = MaterialTheme.typography.titleLarge
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "account"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.Share,
+                    contentDescription = "account"
+                )
+            }
+        },
+        scrollBehavior = scrollBehavior
+    )
 }
 
 @ExperimentalFoundationApi
