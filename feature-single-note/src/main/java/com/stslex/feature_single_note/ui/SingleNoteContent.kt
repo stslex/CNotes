f62d482f55c578a1d12d2a404d0e03067cdd3770
Ilya Stepanyuk <ilya977.077@gmail.com>
2022-05-29 01:12:14 +0300

    refactor SingleNote -> add BackHandler (#5)

--- feature-single-note/src/main/java/com/stslex/feature_single_note/ui/SingleNoteContent.kt
+++ feature-single-note/src/main/java/com/stslex/feature_single_note/ui/SingleNoteContent.kt
@@ -2,6 +2,8 @@ package com.stslex.feature_single_note.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -10,7 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.stslex.core.ValueState
+import androidx.compose.ui.unit.dp
 import com.stslex.core_model.model.NoteUI
 import com.stslex.core_ui.theme.AppTheme
 import com.stslex.feature_single_note.ui.components.SingleNoteTextInputContent
@@ -19,31 +21,43 @@ import com.stslex.feature_single_note.ui.components.SingleNoteTextInputTitle
 @Composable
 fun SingleNoteContent(
     modifier: Modifier = Modifier,
-    valueState: State<ValueState<NoteUI>>
+    noteState: State<NoteUI>
 ) {
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .padding(20.dp)
     ) {
-        when (val result = valueState.value) {
-            is ValueState.Success -> {
-                val note = remember {
-                    mutableStateOf(result.data)
-                }
-                SingleNoteTextInputTitle(note = note)
-                SingleNoteTextInputContent(note = note)
-            }
-            is ValueState.Failure -> Unit
-            is ValueState.Loading -> Unit
-        }
+        SingleNoteTextInputTitle(
+            modifier = Modifier.fillMaxWidth(),
+            note = noteState
+        )
+        SingleNoteTextInputContent(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 20.dp),
+            note = noteState
+        )
     }
 }
+
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun SingleNotePreview() {
     AppTheme {
         Surface(contentColor = MaterialTheme.colorScheme.background) {
-
+            val valueState = remember {
+                mutableStateOf(
+                    NoteUI(
+                        id = 0,
+                        title = mutableStateOf("Title"),
+                        content = mutableStateOf("Content"),
+                        timestamp = System.currentTimeMillis()
+                    )
+                )
+            }
+            SingleNoteContent(noteState = valueState)
         }
     }
 }
--- feature-single-note/src/main/java/com/stslex/feature_single_note/ui/SingleNoteRoute.kt
+++ feature-single-note/src/main/java/com/stslex/feature_single_note/ui/SingleNoteRoute.kt
@@ -1,66 +1,58 @@
 package com.stslex.feature_single_note.ui
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.stslex.core.ValueState
 import com.stslex.core_model.model.NoteUI
-import com.stslex.feature_single_note.ui.components.SingleNoteSaveFab
 
 @Composable
-fun SingleNoteRoute(
-    popBackStack: () -> Unit,
-    viewModel: SingleNoteViewModel
-) {
+fun SingleNoteRoute(viewModel: SingleNoteViewModel) {
     SingleNoteScreen(
         modifier = Modifier.windowInsetsPadding(WindowInsets.safeDrawing),
-        popBackStack = popBackStack,
         viewModel = viewModel
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SingleNoteScreen(
     modifier: Modifier = Modifier,
-    popBackStack: () -> Unit,
     viewModel: SingleNoteViewModel
 ) {
     val noteState: State<ValueState<NoteUI>> = viewModel.note.collectAsState()
-    Scaffold(
-        modifier = modifier,
-        floatingActionButton = {
-            SingleNoteSaveFab(
-                onClickSave = noteState.onClickSave(viewModel::updateNote, popBackStack)
-            )
-        }
-    ) { paddingValues ->
-        SingleNoteContent(
-            modifier = Modifier.padding(paddingValues),
-            valueState = noteState
-        )
-    }
-}
 
-fun State<ValueState<NoteUI>>.onClickSave(
-    updateNote: (NoteUI) -> Unit,
-    popBackStack: () -> Unit
-): () -> Unit = {
-    when (val result = value) {
-        is ValueState.Success -> with(result.data) {
-            if (getContent().value.isNotEmpty() || getTitle().value.isNotEmpty()) {
-                updateNote(result.data.copy(timestamp = System.currentTimeMillis()))
-                popBackStack()
+    when (val result = noteState.value) {
+        is ValueState.Success -> {
+            val note = remember {
+                mutableStateOf(result.data)
+            }
+            SingleNoteContent(
+                modifier = modifier,
+                noteState = note
+            )
+            BackHandler {
+                with(note.value) {
+                    if (getContent().value.isNotEmpty() || getTitle().value.isNotEmpty()) {
+                        val createdNote = copy(timestamp = System.currentTimeMillis())
+                        viewModel.updateNote(createdNote)
+                    }
+                }
             }
         }
         is ValueState.Failure -> Unit
         is ValueState.Loading -> Unit
+    }
+}
+
+fun State<NoteUI>.onClickSave(
+    updateNote: (NoteUI) -> Unit,
+    popBackStack: () -> Unit
+): () -> Unit = {
+    if (value.getContent().value.isNotEmpty() || value.getTitle().value.isNotEmpty()) {
+        updateNote(value.copy(timestamp = System.currentTimeMillis()))
+        popBackStack()
     }
 }
