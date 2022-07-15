8f05013702b5bd191c625749f47301f5b5f99f25
stslex <ilya977.077@gmail.com>
2022-07-15 22:33:54 +0300

    refactoring

--- build-logic/dependencies/src/main/kotlin/com/stslex/cnotes/KotlinAndroid.kt
+++ build-logic/dependencies/src/main/kotlin/com/stslex/cnotes/KotlinAndroid.kt
@@ -2,15 +2,11 @@ package com.stslex.cnotes
 
 import com.android.build.api.dsl.CommonExtension
 import com.android.build.api.dsl.DefaultConfig
-import com.stslex.cnotes.BuildConstants.OptionFields.KOTLIN_JVM_OPTIONS
-import com.stslex.cnotes.BuildConstants.Paths.LOCAL_PROPERTIES
-import com.stslex.cnotes.BuildConstants.Paths.SOURCE_DIR
-import com.stslex.cnotes.BuildConstants.Types.STRING
-import com.stslex.cnotes.BuildConstants.Values.FIREBASE_API_KEY
-import com.stslex.cnotes.BuildConstants.Values.FIREBASE_APPLICATION_ID
-import com.stslex.cnotes.BuildConstants.Values.FIREBASE_CLIENT_ID
-import com.stslex.cnotes.BuildConstants.Values.FIREBASE_DATABASE_URL
-import com.stslex.cnotes.BuildConstants.Values.FIREBASE_PROJECT_ID
+import com.stslex.cnotes.LocalPropertiesConstants.FIREBASE_API_KEY
+import com.stslex.cnotes.LocalPropertiesConstants.FIREBASE_APPLICATION_ID
+import com.stslex.cnotes.LocalPropertiesConstants.FIREBASE_CLIENT_ID
+import com.stslex.cnotes.LocalPropertiesConstants.FIREBASE_DATABASE_URL
+import com.stslex.cnotes.LocalPropertiesConstants.FIREBASE_PROJECT_ID
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
@@ -30,24 +26,33 @@ fun Project.configureKotlinAndroid(
         defaultConfig {
             minSdk = 26
             Properties().apply {
-                load(project.rootProject.file(LOCAL_PROPERTIES).inputStream())
+                load(project.rootProject.file("local.properties").inputStream())
             }.let(::setUpBuildConstants)
         }
+
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_11
             targetCompatibility = JavaVersion.VERSION_11
             isCoreLibraryDesugaringEnabled = true
         }
-        sourceSets.all {
-            java.srcDirs(SOURCE_DIR)
-        }
-        kotlinOptions {
-            freeCompilerArgs = freeCompilerArgs + KOTLIN_JVM_OPTIONS
-            // Set JVM target to 11
-            jvmTarget = "11"
 
+        kotlinOptions {
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-Xopt-in=kotlin.RequiresOptIn",
+                "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                "-Xopt-in=kotlinx.coroutines.FlowPreview",
+                "-Xopt-in=kotlin.Experimental",
+                "-Xopt-in=kotlinx.serialization.ExperimentalSerializationApi"
+            )
+            jvmTarget = "11"
             sourceSets.all {
                 kotlin.srcDir("build/generated/ksp/$name/kotlin")
+            }
+        }
+
+        packagingOptions {
+            resources {
+                excludes += "/META-INF/{AL2.0,LGPL2.1}"
             }
         }
     }
@@ -64,9 +69,9 @@ private fun CommonExtension<*, *, *, *>.kotlinOptions(block: KotlinJvmOptions.()
 }
 
 private fun DefaultConfig.setUpBuildConstants(properties: Properties): Unit = with(properties) {
-    buildConfigField(STRING, FIREBASE_PROJECT_ID, getProperty(FIREBASE_PROJECT_ID))
-    buildConfigField(STRING, FIREBASE_APPLICATION_ID, getProperty(FIREBASE_APPLICATION_ID))
-    buildConfigField(STRING, FIREBASE_CLIENT_ID, getProperty(FIREBASE_CLIENT_ID))
-    buildConfigField(STRING, FIREBASE_API_KEY, getProperty(FIREBASE_API_KEY))
-    buildConfigField(STRING, FIREBASE_DATABASE_URL, getProperty(FIREBASE_DATABASE_URL))
+    buildConfigField("String", FIREBASE_PROJECT_ID, getProperty(FIREBASE_PROJECT_ID))
+    buildConfigField("String", FIREBASE_APPLICATION_ID, getProperty(FIREBASE_APPLICATION_ID))
+    buildConfigField("String", FIREBASE_CLIENT_ID, getProperty(FIREBASE_CLIENT_ID))
+    buildConfigField("String", FIREBASE_API_KEY, getProperty(FIREBASE_API_KEY))
+    buildConfigField("String", FIREBASE_DATABASE_URL, getProperty(FIREBASE_DATABASE_URL))
 }
---
+++ build-logic/dependencies/src/main/kotlin/com/stslex/cnotes/LocalPropertiesConstants.kt
@@ -0,0 +1,9 @@
+package com.stslex.cnotes
+
+internal object LocalPropertiesConstants {
+    const val FIREBASE_PROJECT_ID = "FIREBASE_PROJECT_ID"
+    const val FIREBASE_APPLICATION_ID = "FIREBASE_APPLICATION_ID"
+    const val FIREBASE_CLIENT_ID = "FIREBASE_CLIENT_ID"
+    const val FIREBASE_API_KEY = "FIREBASE_API_KEY"
+    const val FIREBASE_DATABASE_URL = "FIREBASE_DATABASE_URL"
+}
