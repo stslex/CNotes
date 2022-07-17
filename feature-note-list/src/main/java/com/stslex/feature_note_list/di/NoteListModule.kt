a51846722407af6c5b66e888735f234bdbc5adf7
stslex <ilya977.077@gmail.com>
2022-07-17 14:02:09 -0400

    refactor

--- feature-note-list/src/main/java/com/stslex/feature_note_list/di/NoteListModule.kt
+++ feature-note-list/src/main/java/com/stslex/feature_note_list/di/NoteListModule.kt
@@ -1,10 +1,10 @@
 package com.stslex.feature_note_list.di
 
 import androidx.paging.PagingConfig
+import com.example.feature_note_list.ui.NotesViewModel
+import com.stslex.core_model.common.MapperName
 import com.stslex.feature_note_list.data.abstraction.NoteListRepository
 import com.stslex.feature_note_list.data.realisation.NoteListRepositoryImpl
-import com.stslex.feature_note_list.ui.NotesViewModel
-import com.stslex.core_model.common.MapperName
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
